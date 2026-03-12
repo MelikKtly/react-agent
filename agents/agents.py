23b23154ca9f3e9405b9362d061b451cb703a85f
MelikKtly <melikatilla@hotmail.com>
2026-03-12 03:30:06 +0300

    second agent

--- agents/agents.py
+++ agents/agents.py
@@ -21,5 +21,32 @@ word -> language
 """
 )
 
+def main_agent(text:str):
+    prompt=main_prompt.format(text=text)
+    response=llm.invoke(prompt)
+    return response
 
 
+test_prompt=PromptTemplate.from_template(
+"""
+You are a validator agent.
+
+Original message:
+{text}
+
+Language detection result:
+{result}
+
+Check if the analysis is correct.
+
+Return:
+Correct or Incorrect. If incorrect, explain why shortly.
+
+"""
+)
+
+
+def test_agent(text:str, result:str):
+    prompt=test_prompt.format(text=text, result=result)
+    response=llm.invoke(prompt)
+    return response
